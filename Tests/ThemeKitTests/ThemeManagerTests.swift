45ea3e0fb8d712cb4cbe942183c030151668ff44
Paul Schifferer <paul@schifferers.net>
2018-09-02 20:00:40 -0700

    Add tests for ThemeManager.

--- Tests/ThemeKitTests/ThemeManagerTests.swift
+++ Tests/ThemeKitTests/ThemeManagerTests.swift
@@ -7,7 +7,10 @@
 
 import XCTest
 
-class ThemeManagerTests: XCTestCase {
+@testable import ThemeKit
+
+
+class ThemeManagerTests : XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,16 +20,39 @@ class ThemeManagerTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInitialTheme() {
+        let thm = ThemeManager()
+
+        let theme = thm.currentTheme
+
+        XCTAssertTrue(theme.id == ThemeIdentifier.default.rawValue, "Expected \(ThemeIdentifier.default.rawValue) for current theme; got \(theme.id).")
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testAddTheme() throws {
+        let thm = ThemeManager()
+
+        let theme = Theme(id: "test1", name: "Test 1")
+        try thm.save(theme: theme)
+
+        let t = thm.theme(id: "test1")
+        XCTAssertNotNil(t)
+        XCTAssertTrue(t?.id == "test1", "Expected theme 'test1', got \(String(describing: t?.id)).")
+
+        let themes = thm.allThemes()
+        XCTAssertTrue(themes.count == 1, "Expected 1 theme, got \(themes.count).")
+    }
+
+    func testCurrentTheme() {
+        let thm = ThemeManager()
+
+        let t1 = thm.currentTheme
+        XCTAssertTrue(t1.id == ThemeIdentifier.default.rawValue, "Expected theme '\(ThemeIdentifier.default.rawValue)', got '\(t1.id)'.")
+
+        let theme = Theme(id: "test1", name: "Test 1")
+        thm.currentTheme = theme
+
+        let t2 = thm.currentTheme
+        XCTAssertTrue(t2.id == "test1", "Expected theme 'test1', got '\(t2.id)'.")
     }
 
 }
